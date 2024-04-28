c668136491994456819bd7dc046dc3a4538c60d8
Prapti Sinha <109864799+SanyaSinha@users.noreply.github.com>
2024-04-28 20:23:56 +0530

    Update Addition.java

--- Addition.java
+++ Addition.java
@@ -1,7 +1,7 @@
-public class Addition{
-    public static void main(String args[]){
-        int a=10 , b=90 , sum;
-        sum=a+b;
-        System.out.println("Sum is " +sum);
+public class Addition { // Class declaration
+    public static void main(String args[]) { // Main method
+        int a = 10, b = 90, sum; // Declaring and initializing variables a and b
+        sum = a + b; // Computing the sum of a and b and storing it in sum
+        System.out.println("Sum is " + sum); // Printing the sum
     }
 }
